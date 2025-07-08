1d027511daf405b96dfddc7af999195b2e05db1e
Manuel <20220416@ricaldone.edu.sv>
2025-07-08 10:54:30 -0600

    creo que ya termine

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 import express from "express";
 import corse from "cors"; 
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+import cors from "cors"
+
+
 
 import clientesRoutes from "./src/routes/clientes.js";
 import reservacionesRoutes from "./src/routes/reservaciones.js";
 
 const app = express();
+const swaggerDocumment = JSON.parse(
+
+    fs.readFileSync(path.resolve("./docs.json"), "utf8")
+);
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumment));
+app.use(
+    cors({
+      origin: "https://apiplus.onrender.com", 
+      credentials: true
+    })
+  );
 
 app.use(corse());
 app.use(express.json());
